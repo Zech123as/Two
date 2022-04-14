c301cab63792a15b0158334044c0fde0695774c1
Zech123as <97283406+Zech123as@users.noreply.github.com>
2022-04-14 13:04:39 +0530

    Update Two.py

--- Two.py
+++ Two.py
@@ -154,7 +154,7 @@ for Expiry_Dist in range(Expiry_Dist_input[0], Expiry_Dist_input[1] + 1, 1):
 			Final_DF["CE_BUY"]  = "CE (" + str(round(ce_buy_entry)).rjust(5) + " |" + ce_buy['o'].round().astype(int).astype(str).str.rjust(5) + " )"
 			Final_DF["PE_BUY"]  = "PE (" + str(round(pe_buy_entry)).rjust(5) + " |" + pe_buy['o'].round().astype(int).astype(str).str.rjust(5) + " )"	
 
-			Final_DF["FINAL"] = f'{Final_DF["CE_SELL"]}   |   {Final_DF["PE_SELL"]} \n( {Final_DF["CE_BUY"]}   |   {Final_DF["PE_BUY"]} )  * {str(Buy_Lots)}'
+			Final_DF["FINAL"] = Final_DF["CE_SELL"] + "   |   " + Final_DF["PE_SELL"] +"   |   " + "\n( " + Final_DF["CE_BUY"] + "   |   " + Final_DF["PE_BUY"] + " ) " + " * " + str(Buy_Lots)
 
 			if Final_DF['Change' + str(Sell_Dist)].max() > Max_Profit:
 				Max_Profit = Final_DF['Change' + str(Sell_Dist)].max()
