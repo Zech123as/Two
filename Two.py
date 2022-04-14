5bce097e0d3c0c9e6072a14599e1cbc0b8937270
Zech123as <97283406+Zech123as@users.noreply.github.com>
2022-04-14 15:28:08 +0530

    Update Two.py

--- Two.py
+++ Two.py
@@ -179,7 +179,7 @@ for Expiry_Dist in range(Expiry_Dist_input[0], Expiry_Dist_input[1] + 1, 1):
 				
 			Final_DF
 
-			fig_dict[Sell_Dist].add_trace(go.Scatter(x=Final_DF.index, y=Final_DF["Change"+str(Sell_Dist)], legendrank = Group_Rank, mode = 'lines', legendgrouptitle_text = Legend_Group, legendgroup= Legend_Group, customdata = Final_DF, name = str(end_time_input.date()).rjust(10), hovertemplate='Profit: (%{y:5d} )   |   %{customdata[7]}<br>%{customdata[8]}'))
+			fig_dict[Sell_Dist].add_trace(go.Scatter(x=Final_DF.index, y=Final_DF["Change"+str(Sell_Dist)], legendrank = Group_Rank, mode = 'lines', legendgrouptitle_text = Legend_Group, legendgroup= Legend_Group, customdata = Final_DF, name = str(end_time_input.date()).rjust(10), hovertemplate='%{customdata[7]}<br>%{customdata[8]}'))
 
 
 for Sell_Dist in range((Sell_Dist_input)[0], (Sell_Dist_input)[1]+1, 1):
